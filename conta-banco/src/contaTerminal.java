9c4b8a1ad762ec5c5b418acb2ac4b426fe07f3f6
Adriano <adriano.me283@gmail.com>
2024-06-14 21:09:22 -0300

    conta-terminal

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,21 @@
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello");
+        //TODO:cconhecer e importar a classe scanner
+
+        //Exibir as mensagens para o nosso usuario
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+       String agencia = args [0];
+       String usuario = args[1];
+       String nome = args [2];
+       String saldo = args [3];
+
+       System.out.println("Digite sua conta " + agencia);
+       System.out.println("digite seu numero de usuario " + usuario);
+       System.out.println("digite seu nome " + nome);
+       System.out.println("obrigado por criar sua conta, seu saldo é: " + saldo);
+
     }
 }
